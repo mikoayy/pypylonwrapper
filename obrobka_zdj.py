de63def2ebb7108ade2a7b120d109ff2844335c7
mikoayy <mikolaj20031221@gmail.com>
2025-06-23 20:35:05 +0200

    Update obrobka_zdj.py

--- obrobka_zdj.py
+++ obrobka_zdj.py
@@ -1,18 +1,23 @@
-from pypylon_wraper import load_images, merge
+from pypylon_wraper import load_images, merge, get_photos_paths
 
 configs={
     "blur_ksize": (5,5),
+    "blur_sigma": 0.0,
     "strenght": 1.5,
     "treshold": 35,
     "closing_kernel": (17,17),
-    "max_workers": 16 
+    "max_workers": 16
 }
-img_zdatne = load_images("/home/mikoay/Documents/zdatne",max_workers=16)
-img_niezdatne = load_images("/home/mikoay/Documents/niezdatne",max_workers=16)
-all_imgs = merge(img_zdatne,img_niezdatne)
 
-print(img_zdatne.shape)
-print(img_niezdatne.shape)
+img_paths_new = get_photos_paths("/home/mikoay/Documents/zdatne")
+img_paths_used = get_photos_paths("/home/mikoay/Documents/niezdatne")
+
+img_new = load_images(img_paths_new,max_workers=16)
+img_used = load_images(img_paths_used,max_workers=16)
+all_imgs = merge(img_new,img_used)
+
+print(img_new.shape)
+print(img_used.shape)
 print(all_imgs.shape)
 
 all_imgs.pipeline(**configs)
